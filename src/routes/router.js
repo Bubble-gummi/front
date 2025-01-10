d5ea55a715186d7ed6f1e7aee2d91e7da60d1b75
suwon1234 <psj04100410262635@gmail.com>
2025-01-10 11:36:39 +0900

    라우터 수정

--- src/routes/router.js
+++ src/routes/router.js
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element : <WeekMovieContainer/>
       },
       {
-        path : "/movie/moviereview/",
+        path : "/movie/moviereview/:id",
         element : <MovieReviewContainer/>
       },
       {
